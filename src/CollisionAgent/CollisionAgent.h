7f5f1fb17085f330ef7e8358019b93a66800ec69
Nick <avinanick@gmail.com>
2020-04-18 20:10:04 -0700

    Started creating the header file for the collision agent that will be used by units that need to avoid other units.

---
+++ src/CollisionAgent/CollisionAgent.h
@@ -0,0 +1,29 @@
+#ifndef COLLISIONAGENT_H
+#define COLLISIONAGENT_H
+
+#include <Godot.hpp>
+#include <Node.hpp>
+#include "../RVO.h"
+
+namespace godot {
+
+class CollisionAgent: public Node {
+    GODOT_CLASS(CollisionAgent, Node)
+
+private:
+    int agent_number = 0;
+
+public:
+    static void _register_methods();
+
+    CollisionAgent();
+    ~CollisionAgent();
+
+    void _init();
+
+    void _process(float delta);
+}
+
+}
+
+#endif
