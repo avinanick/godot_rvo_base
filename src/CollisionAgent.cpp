f13362ff1034853cc54b88d4676bb8e17796c3d3
Nick <avinanick@gmail.com>
2020-04-25 23:51:41 -0700

    Updated the collision agent with another signal for reaching the goal, and the method for setting up the agent. I'm not sure how else is best to handle the setup yet, since I don't want this directly accessing it's parent. Also updated the response to it's position update, so it also updates it's preferred velocity to the server.

--- src/CollisionAgent.cpp
+++ src/CollisionAgent.cpp
@@ -5,8 +5,10 @@ using namespace godot;
 void CollisionAgent::_register_methods() {
     register_method("_process", &CollisionAgent::_process);
     register_method("set_goal", &CollisionAgent::set_goal);
+    register_method("setup_agent", &CollisionAgent::setup_agent);
 
     register_signal<CollisionAgent>((char *)"position_updated", "new_position", GODOT_VARIANT_TYPE_VECTOR2);
+    register_signal<CollisionAgent>((char *)"goal_reached");
 }
 
 CollisionAgent::CollisionAgent() {
@@ -40,4 +42,14 @@ void CollisionAgent::setup_agent(float x_position, float y_position, float agent
 void CollisionAgent::update_agent_position() {
     agent_position = collision_server->get_agent_position(agent_number);
     emit_signal("position_updated", agent_position);
+    float dist_to_goal = agent_position.distance_squared_to(agent_goal);
+    if(dist_to_goal < (agent_radius * agent_radius)) {
+        // reached the goal
+        emit_signal("goal_reached");
+        collision_server->set_agent_preferred_velocity(agent_number, 0.0, 0.0);
+    }
+    else {
+        Vector2 preferred_velocity = (agent_position - agent_goal).normalized();
+        collision_server->set_agent_preferred_velocity(agent_number, preferred_velocity.x, preferred_velocity.y);
+    }
 }
